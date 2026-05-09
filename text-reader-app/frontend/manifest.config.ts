7df117e28356cbb7ce0959aa6341cd5d8963a584
Aahil <aahillegend46@gmail.com>
2026-05-09 16:05:27 -0600

    Build Bonita extension frontend

--- text-reader-app/frontend/manifest.config.ts
+++ text-reader-app/frontend/manifest.config.ts
@@ -3,14 +3,17 @@ import pkg from './package.json'
 
 export default defineManifest({
   manifest_version: 3,
-  name: pkg.name,
+  name: 'Bonita',
+  description: 'A calm reading overlay for dense web content.',
   version: pkg.version,
   icons: {
     48: 'public/logo.png',
+    128: 'public/logo-128.png',
   },
   action: {
     default_icon: {
       48: 'public/logo.png',
+      128: 'public/logo-128.png',
     },
     default_popup: 'src/popup/index.html',
   },
@@ -21,7 +24,7 @@ export default defineManifest({
   ],
   content_scripts: [{
     js: ['src/content/main.tsx'],
-    matches: ['https://*/*'],
+    matches: ['http://*/*', 'https://*/*'],
   }],
   side_panel: {
     default_path: 'src/sidepanel/index.html',
@@ -29,7 +32,7 @@ export default defineManifest({
   web_accessible_resources: [
     {
       resources: ['fonts/*'],
-      matches: ['https://*/*'],
+      matches: ['http://*/*', 'https://*/*'],
     },
   ],
 })
